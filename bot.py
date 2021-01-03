b00148b5f7a8260b189f479c08168c67cc967b35
RabbidTurkey <76893259+RabbidTurkey@users.noreply.github.com>
2021-01-03 15:22:04 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,31 @@
 import discord
-from discord.ext import commands
+import time
 
-client=commands.Bot(command_prefix='!')
+client = discord.Client()
+
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print('Logged on as', client.user)
 
 @client.event
-async def on_member_join(member):
-    print("A wild",member,"appeared")
+async def on_message( message):
+    # don't respond to ourselves
+    if message.author == client.user:
+        return
+    elif "ping" in message.content.lower():
+        t = time.time()
+        msg_ = await message.channel.send('Pong!')
+        await msg_.edit(content = 'Pong! `{} ms`'.format(int((time.time()-t)*1000)))
+    elif "pls rob" in message.content.lower():
+        await message.channel.send(':x: **This command is disabled because of users simply joining to steal from the richest person, and then leaving.**')
+    elif "pls heist" in message.content.lower():
+        await message.channel.send(':x: **This command is disabled because of users simply joining to steal from the richest person, and then leaving.**')
+    elif "poggers" in message.content.lower():
+        await message.add_reaction("a:TE_PogEater:787019787829116979")
 
-@client.event
-async def on_member_remove(member):
-    print("The wild",member,"disappeared")
+    # Setting `Playing ` status
+        await client.change_presence(activity=discord.Game(name="Updating"))
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"Hey! I am up and running with a ping of {round(client.latency*1000)}ms.")
 
-client.run('')   #Place token between the ''
+
+client.run('Nzk0MDI0MjgzNTQ5NDY2NjM0.X-0yvQ.G8prbG-UKaTPRbc-CAz9bCZrVhA')
